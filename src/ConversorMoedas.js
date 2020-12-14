26809f114dba6b83593c39c86591f940f15059bc
Arthur Cezar <arthurcvm@hotmail.com>
2020-12-14 16:40:27 -0300

    Implementação da modal

--- src/ConversorMoedas.js
+++ src/ConversorMoedas.js
@@ -21,6 +21,8 @@ function ConversorMoedas() {
   const [moedaPara, setMoedaPara] = useState("USD");
   const [exibirSpinner, setExibirSpinner] = useState(false);
   const [formValidado, setFormValidado] = useState(false);
+  const [exibirModal, setExibirModal] = useState(false);
+  const [resultadoConversao, setResultadoConversao] = useState("");
 
   function handleValor(event) {
     setValor(event.target.value.replace(/\D/g, ""));
@@ -34,10 +36,19 @@ function ConversorMoedas() {
     setMoedaPara(event.target.value);
   }
 
+  function handleFecharModal(event) {
+    setValor("1");
+    setMoedaDe("BRL");
+    setMoedaPara("USD");
+    setFormValidado(false);
+    setExibirModal(false);
+  }
+
   function converter(event) {
     event.preventDefault();
     setFormValidado(true);
     if (event.currentTarget.checkValidity() === true) {
+      setExibirModal(true);
     }
   }
 
@@ -91,13 +102,15 @@ function ConversorMoedas() {
             </Col>
           </Form.Row>
         </Form>
-        <Modal show={false}>
+        <Modal show={exibirModal} onHide={handleFecharModal}>
           <Modal.Header closeButton>
             <Modal.Title>Conversão</Modal.Title>
           </Modal.Header>
-          <Modal.Body>Resultado da conversão aqui...</Modal.Body>
+          <Modal.Body>{resultadoConversao}</Modal.Body>
           <Modal.Footer>
-            <Button variant="success">Nova conversão</Button>
+            <Button variant="success" onClick={handleFecharModal}>
+              Nova conversão
+            </Button>
           </Modal.Footer>
         </Modal>
       </Jumbotron>

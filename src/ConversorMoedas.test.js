6047f6189c3402d85ff1f763f3ea103cf82f091c
Arthur Cezar <arthurcvm@hotmail.com>
2020-12-15 16:57:53 -0300

    Testes unitários com mockagem de dados do axios

--- src/ConversorMoedas.test.js
+++ src/ConversorMoedas.test.js
@@ -1,10 +1,26 @@
-import { render, screen } from "@testing-library/react";
+import { library } from "@fortawesome/fontawesome-svg-core";
 import React from "react";
 import ReactDOM from "react-dom";
 import ConversorMoedas from "./ConversorMoedas";
+import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import axiosMocks from "axios";
 
-test("Deve renderizar o componente sem erros", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<ConversorMoedas />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("Teste do componente de conversão de moedas", () => {
+  it("Deve renderizar o componente sem erros", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<ConversorMoedas />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it("Deve simular uma conversão de moedas", async () => {
+    const { findByTestId, getByTestId } = render(<ConversorMoedas />);
+    axiosMocks.get.mockResolvedValueOnce({
+      data: { success: true, rates: { BRL: 4.564292, USD: 1.101049 } },
+    });
+    fireEvent.click(getByTestId("btn-converter"));
+    const modal = await findByTestId("modal");
+    expect(axiosMocks.get).toHaveBeenCalledTimes(1);
+    expect(modal).toHaveTextContent("1 BRL = 0.24 USD");
+  });
 });
